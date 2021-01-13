dee765da7b21385ddc6dc28ecb82d2efad58444e
guofuqiang00 <gfqjava@163.com>
2021-01-13 14:12:01 +0800

    update 1

---
+++ src/views/test/parentChild/child/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="child">
+    <el-alert
+      title="新密码:"
+      type="success">
+      <span>{{password}}</span>
+      <!-- 可直接使用从父组件接收的数据 -->
+    </el-alert>
+  </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                show: false,
+            };
+        },
+        props: {
+            password: String
+            // 声明一个自定义的属性
+        },
+    }
+</script>
+
